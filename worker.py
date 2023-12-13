636e08d91dd02188be2b99c1bba0a932f4535adb
lediontud <124886613+lediontud@users.noreply.github.com>
2023-12-13 13:47:51 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import requests
 import os
 import json
+from google.auth.transport.requests import Request
+from google.auth import exceptions
+from google.oauth2 import service_account
+
 app = Flask(__name__)
 
 def get_api_key() -> str:
@@ -10,44 +13,44 @@ def get_api_key() -> str:
     if secret:
         return secret
     else:
-        #local testing
-        with open('.key') as f:
-            return f.read().strip()
-      
+        # Local testing
+        credentials_path = 'key.json'
+        credentials, _ = service_account.Credentials.from_service_account_file(
+            credentials_path,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"]
+        ).with_request(Request())
+        return credentials.token
+
 @app.route("/")
 def hello():
     return "Add workers to the Spark cluster with a POST request to add"
 
 @app.route("/test")
 def test():
-    #return "Test" # testing 
-    return(get_api_key())
+    return get_api_key()
 
-@app.route("/add",methods=['GET','POST'])
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-  if request.method=='GET':
-    return "Use post to add" # replace with form template
-  else:
-    token=get_api_key()
-    ret = addWorker(token,request.form['num'])
-    return ret
-
+    if request.method == 'GET':
+        return "Use post to add"  # replace with form template
+    else:
+        token = get_api_key()
+        ret = addWorker(token, request.form['num'])
+        return ret
 
 def addWorker(token, num):
     with open('payload.json') as p:
-      tdata=json.load(p)
-    tdata['name']='slave'+str(num)
-    data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/feisty-tempest-406610/zones/europe-west2-b/instances'
-    headers={"Authorization": "Bearer "+token}
-    resp=requests.post(url,headers=headers, data=data)
-    if resp.status_code==200:     
-      return "Done"
+        tdata = json.load(p)
+    tdata['name'] = 'slave' + str(num)
+    data = json.dumps(tdata)
+    url = 'https://www.googleapis.com/compute/v1/projects/feisty-tempest-406610/zones/europe-west2-b/instances'
+    headers = {"Authorization": "Bearer " + token}
+    resp = requests.post(url, headers=headers, data=data)
+    if resp.status_code == 200:
+        return "Done"
     else:
-      print(resp.content)
-      return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
-
-
+        print(resp.content)
+        return "Error\n" + resp.content.decode('utf-8') + '\n\n\n' + data
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0', port='8080')
